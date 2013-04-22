10178de6c93b8e8cf7752f44ca2d02d94742dee6
ananes77 <ananes77@hotmail.com>
2013-04-21 22:11:52 -0500

    Se actualiza Text Resize 7.x-1.8

--- sites/all/modules/text_resize/text_resize.js
+++ sites/all/modules/text_resize/text_resize.js
@@ -1,17 +1,16 @@
-// $Id: text_resize.js,v 1.1.2.8.2.6 2010/12/29 23:14:13 attheshow Exp $
 (function ($) { // JavaScript should be compatible with other libraries than jQuery
   Drupal.behaviors.textResize = { // D7 "Changed Drupal.behaviors to objects having the methods "attach" and "detach"."
     attach: function(context) {
       // Which div or page element are we resizing?
       if (text_resize_scope) { // Admin-specified scope takes precedence.
         if ($('#'+text_resize_scope).length > 0) {
-          var element_to_resize = $('.panel-panel p,.panel-panel h2,.panel-panel ul,.panel-panel h1, .panel-panel  table,.panel-panel  tr,.panel-panel td,.panel-panel  th,.panel-panel  div,.panel-panel  a'); // ID specified by admin
+          var element_to_resize = $('#'+text_resize_scope); // ID specified by admin
         }
         else if ($('.'+text_resize_scope).length > 0) {
-          var element_to_resize = $('.panel-panel p,.panel-panel h2,.panel-panel ul,.panel-panel h1, .panel-panel  table, .panel-panel  tr,.panel-panel td,.panel-panel  th,.panel-panel  div,.panel-panel  a'); // CLASS specified by admin
+          var element_to_resize = $('.'+text_resize_scope); // CLASS specified by admin
         }
         else {
-          var element_to_resize = $('.panel-panel p,.panel-panel h2,.panel-panel ul,.panel-panel h1, .panel-panel  table,.panel-panel  tr,.panel-panel td,.panel-panel  th,.panel-panel  div,.panel-panel  a'); // It's just a tag specified by admin
+          var element_to_resize = $(text_resize_scope); // It's just a tag specified by admin
         }
       }
       else { // Look for some default scopes that might exist.
@@ -26,9 +25,16 @@
         }
       }
       // Set the initial font size if necessary
-      
-     
-      // Changer links will change the text size when clicked   element_to_resize = "[$('p'), $('ul'), $('li'),$('a'),$('h1'),$('h2'),$('h3'),$('h4'),$('h5,h6, strong]";
+      if ($.cookie('text_resize') != null) {
+        element_to_resize.css('font-size', parseFloat($.cookie('text_resize')) + 'px');
+      }
+      if (text_resize_line_height_allow) {
+        // Set the initial line height if necessary
+        if ($.cookie('text_resize_line_height') != null) {
+          element_to_resize.css('line-height', parseFloat($.cookie('text_resize_line_height')) + 'px');
+        }
+      }
+      // Changer links will change the text size when clicked
       $('a.changer').click(function() {
         // Set the current font size of the specified section as a variable
         var currentFontSize = parseFloat(element_to_resize.css('font-size'), 10);
@@ -40,13 +46,13 @@
           if (text_resize_line_height_allow) { var new_line_height = current_line_height * 1.2; }
           // Allow resizing as long as font size doesn't go above text_resize_maximum.
           if (new_font_size <= text_resize_maximum) {
-            
-            
+            $.cookie('text_resize', new_font_size, { path: '/' });
+            if (text_resize_line_height_allow) { $.cookie('text_resize_line_height', new_line_height, { path: '/' }); }
             var allow_change = true;
           }
           else {
-            
-            
+            $.cookie('text_resize', text_resize_maximum, { path: '/' });
+            if (text_resize_line_height_allow) { $.cookie('text_resize_line_height', text_resize_line_height_max, { path: '/' }); }
             var reset_size_max = true;
           }
         }
@@ -55,20 +61,20 @@
           if (text_resize_line_height_allow) { var new_line_height = current_line_height / 1.2; }
           if (new_font_size >= text_resize_minimum) {
             // Allow resizing as long as font size doesn't go below text_resize_minimum.
-          
-          
+            $.cookie('text_resize', new_font_size, { path: '/' });
+            if (text_resize_line_height_allow) { $.cookie('text_resize_line_height', new_line_height, { path: '/' }); }
             var allow_change = true;
           }
           else {
             // If it goes below text_resize_minimum, just leave it at text_resize_minimum.
-            
-
+            $.cookie('text_resize', text_resize_minimum, { path: '/' });
+            if (text_resize_line_height_allow) { $.cookie('text_resize_line_height', text_resize_line_height_min, { path: '/' }); }
             var reset_size_min = true;
           }
         }
         else if (this.id == 'text_resize_reset') {
-          
-
+          $.cookie('text_resize', null, { path: '/' });
+          if (text_resize_line_height_allow) { $.cookie('text_resize_line_height', null, { path: '/' }); }
           var reset_size_original = true;
         }
         // jQuery lets us set the font size value of the main text div
@@ -88,8 +94,8 @@
           return false;
         }
         else if (reset_size_original == true) {
-          element_to_resize.css('font-size', null);
-          if (text_resize_line_height_allow) { element_to_resize.css('line-height', null); }
+          element_to_resize.css('font-size', '');
+          if (text_resize_line_height_allow) { element_to_resize.css('line-height', ''); }
           return false;
         }
       });
